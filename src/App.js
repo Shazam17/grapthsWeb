44ca4d13cdb306c5b3672c3cb2ce6c75c1dc3d36
John Power <superpozitiv71@gmail.com>
2019-12-15 16:03:24 +0700

    add arrows

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import './App.css';
 
 class Node  {
 
+
+
   constructor(props){
     this.x = props.X;
     this.y = props.Y;
     this.value = props.val;
+    this.selected = false;
   }
 
 }
@@ -44,37 +47,55 @@ class AdjecencyList {
   }
 
   findNode(x ,y){
-      this.list.forEach((elem) =>{
+    let ind = null;
+      this.list.forEach((elem , index) =>{
           if(this.distPoints(x , y , elem.node.x , elem.node.y) < 50){
             console.log(`find point with value ${elem.node.value}`);
+            elem.node.selected = !elem.node.selected;
+            ind = index; 
           }
       });
+      return ind;
   }
 }
+
+
+
 class App extends Component {
 
-  state = {
-    st : "oldState"
-  }
+ 
 
 
   constructor(props){
     super(props);
-    this.list = new AdjecencyList();
+    this. state = {
+      selected : null,
+      list : new AdjecencyList()
+    };
     document.addEventListener('contextmenu', function(event){
       event.preventDefault();});
 
     document.addEventListener('mousedown',(event)=>{
+      event.preventDefault();
+      let oldSelected = this.state.selected;
       if(event.which === 3){
-        this.list.addNode(new Node({X:event.clientX , Y:event.clientY }));
+        this.state.list.addNode(new Node({X:event.clientX , Y:event.clientY }));
       }
 
       if(event.which === 1){
-       this.list.findNode(event.clientX  , event.clientY );
+        let selected = this.state.list.findNode(event.clientX  , event.clientY);
+        if(selected !== oldSelected && oldSelected != null){
+          this.state.list.list[oldSelected].pushNode(this.state.list.list[selected].node);
+          oldSelected = null;
+        }else{
+            oldSelected = selected;
+        }
+        
+        console.log(selected);
       }
       this.setState(()=>{
         console.log("update");
-        return {st: "new State"};
+        return {list : this.state.list , selected :oldSelected };
       })
     });
    
@@ -82,12 +103,36 @@ class App extends Component {
   }
   
   renderLines(){
-
+    if(this.state.list.list.length != 0){
+      const ctx = this.refs.canvas.getContext('2d');
+      this.state.list.list.forEach((elem) =>{
+        let node = elem.node;
+        elem.list.forEach((elTo) =>{
+          // ctx.beginPath();
+          // ctx.moveTo(node.x, node.y);
+          // ctx.lineTo(elTo.x , elTo.y);
+          // ctx.lineWidth = 10;
+          // ctx.stroke();
+          this.canvas_arrow(ctx , node.x ,node.y , elTo.x , elTo.y);
+          ctx.stroke();
+        });
+    });
+    }
+    
   }
-
+  canvas_arrow(context, fromx, fromy, tox, toy) {
+    var headlen = 20; // length of head in pixels
+    var dx = tox - fromx;
+    var dy = toy - fromy;
+    var angle = Math.atan2(dy, dx);
+    context.moveTo(fromx, fromy);
+    context.lineTo(tox, toy);
+    context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+    context.moveTo(tox, toy);
+    context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+  }
   renderNodes(){
-    console.log(this.list.list);
-    return this.list.list.map(elem => (<div style={{
+    return this.state.list.list.map(elem => (<div style={{
       position:"absolute" ,left :elem.node.x ,top:elem.node.y ,
       width:'100px',height: '100px',display:'block',
       background: "#AA2323",
@@ -99,15 +144,20 @@ class App extends Component {
 
   updateCanvas(){
     console.log("updateCanvas");
-    if(this.list.list.length != 0){
+    if(this.state.list.list.length != 0){
       const ctx = this.refs.canvas.getContext('2d');
       ctx.canvas.width  = window.innerWidth;
       ctx.canvas.height = window.innerHeight;
       ctx.clearRect(0, 0,  this.refs.canvas.width,  this.refs.canvas.height);
-      this.list.list.map( elem => {
+      this.state.list.list.map( (elem,index) => {
         ctx.beginPath();
         ctx.arc(elem.node.x, elem.node.y, 50, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'green';
+
+        if(index === this.state.selected){
+          ctx.fillStyle = 'red';
+        }else{
+          ctx.fillStyle = 'green';
+        }
         ctx.fill();
       });
     }
@@ -116,6 +166,7 @@ class App extends Component {
 
   render(){
     this.updateCanvas();
+    this.renderLines();
      return (
     <canvas ref="canvas"></canvas>
     );
